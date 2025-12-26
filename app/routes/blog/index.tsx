d032f4030e7f5fced55c76a349be4fcb01e89e57
Juhasz Bence <juhbence90@gmail.com>
2025-12-26 12:02:13 +0100

    About page, contact page, not found page added

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,19 @@
-import type { Route } from "./+types";
+import type { Route } from "./+types/index";
+import type { PostMeta } from "~/types";
+import { Link } from "react-router";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data = await res.json();
+
+  return { posts: data };
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,7 +22,9 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const BlogPage = () => {
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types";
+import { Form } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,13 +8,97 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const ContactPage = () => {
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const subject = formData.get("subject") as string;
+  const message = formData.get("message") as string;
+  const data = {
+    name,
+    email,
+    subject,
+    message,
+  };
+
+  return { message: "Form submitted successfully", data };
+}
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
       <h2 className="text-3xl font-bold text-white mb-8 text-center">
         📥 Contact Me
       </h2>
-    </>
+
+      {actionData?.message ? (
+        <p className="mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border boder-green-500 shadow-md">
+          {actionData.message}
+        </p>
+      ) : null}
+
+      <Form method="post" className="space-y-6">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subject"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+
+        <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">
+          Send Message
+        </button>
+      </Form>
+    </div>
   );
 };
 
