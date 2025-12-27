ab028489a418e692f34237ea02b0101fd04ecdee
Juhasz Bence <juhbence90@gmail.com>
2025-12-27 11:45:09 +0100

    Blog post page, blog post details page, sort, pagination, filter added

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
-import { Link } from "react-router";
+import PostCard from "~/components/PostCard";
+import Pagination from "~/components/Pagination";
+import PostFilter from "~/components/PostFilter";
 
 export async function loader({
   request,
@@ -11,6 +14,10 @@ export async function loader({
   if (!res.ok) throw new Error("Failed to fetch data");
 
   const data = await res.json();
+
+  data.sort((a: PostMeta, b: PostMeta) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
 
   return { posts: data };
 }
@@ -23,12 +30,53 @@ export function meta({}: Route.MetaArgs) {
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
+
   const { posts } = loaderData;
 
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLocaleLowerCase();
+    return (
+      post.title.toLocaleLowerCase().includes(query) ||
+      post.excerpt.toLocaleLowerCase().includes(query)
+    );
+  });
+
+  const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
+
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
-    </>
+
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(query) => {
+          setSearchQuery(query);
+          setCurrentPage(1);
+        }}
+      />
+
+      <div className="space-y-8">
+        {currentPosts.length === 0 ? (
+          <p className="text-gray-400 text-center">No posts found</p>
+        ) : (
+          currentPosts.map((post) => <PostCard key={post.slug} post={post} />)
+        )}
+      </div>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
+    </div>
   );
 };
 
